b2b627a69cb6f4a0e92bc5ed84da7b7c149f2eec
suda <github>
2021-11-11 14:33:48 +0900

    適当

--- venv_help_child/help_child/main/views.py
+++ venv_help_child/help_child/main/views.py
@@ -3,72 +3,93 @@ from django.views import generic
 # Create your views h
 
 
-
 class HomeView(generic.TemplateView):
-    template_name="home.html"
+    template_name = "home.html"
+
 
 class LoginView(generic.TemplateView):
-    template_name="login.html"
+    template_name = "login.html"
+
 
 class LocationAdminView(generic.TemplateView):
-    template_name="locationAdmin.html"
+    template_name = "locationAdmin.html"
+
 
 class LocationParentView(generic.TemplateView):
-    template_name="locationParent.html"
+    template_name = "locationParent.html"
+
 
 class LocationConfigView(generic.TemplateView):
-    template_name="locationConfig.html"
+    template_name = "locationConfig.html"
+
 
 class ContactTopView(generic.TemplateView):
-    template_name="contactTop.html"
+    template_name = "contactTop.html"
+
 
 class ContactDetailView(generic.TemplateView):
-    template_name="contactDetail.html"
+    template_name = "contactDetail.html"
+
 
 class ContactUpdateView(generic.TemplateView):
-    template_name="contactUpdate.html"
+    template_name = "contactUpdate.html"
+
 
 class MessageAddressView(generic.TemplateView):
-    template_name="messageAddress.html"
+    template_name = "messageAddress.html"
+
 
 class MessageView(generic.TemplateView):
-    template_name="message.html"
+    template_name = "message.html"
+
 
 class AttendView(generic.TemplateView):
-    template_name="attend.html"
+    template_name = "attend.html"
+
 
 class TagScanView(generic.TemplateView):
-    template_name="tagScan.html"
+    template_name = "tagScan.html"
+
 
 class NameListView(generic.TemplateView):
-    template_name="nameList.html"
+    template_name = "nameList.html"
+
 
 class NameListDetailView(generic.TemplateView):
-    template_name="nameListDetail.html"
+    template_name = "nameListDetail.html"
+
 
 class NameListAddView(generic.TemplateView):
-    template_name="nameListAdd.html"
+    template_name = "nameListAdd.html"
+
 
 class NameListUpdateView(generic.TemplateView):
-    template_name="nameListUpdate.html"
+    template_name = "nameListUpdate.html"
+
 
 class PlanListView(generic.TemplateView):
-    template_name="planList.html"
+    template_name = "planList.html"
+
 
 class PlanListDetailView(generic.TemplateView):
-    template_name="planListDetail.html"
+    template_name = "planListDetail.html"
+
 
 class PlanListAddView(generic.TemplateView):
-    template_name="planListAdd.html"
+    template_name = "planListAdd.html"
+
 
 class PlanListUpdateView(generic.TemplateView):
-    template_name="planListUpdate.html"
+    template_name = "planListUpdate.html"
+
 
 class PlanListDeleteView(generic.TemplateView):
-    template_name="planListDelete.html"
+    template_name = "planListDelete.html"
+
 
 class ParentConfigView(generic.TemplateView):
-    template_name="parentConfig.html"
+    template_name = "parentConfig.html"
+
 
 class TeacherConfigView(generic.TemplateView):
-    template_name="teacherConfig.html"
+    template_name = "teacherConfig.html"
