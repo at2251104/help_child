351f3a7beaa4b830ae5a9d842405be554a4f0502
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2021-11-25 12:05:53 +0900

    a

--- venv_help_child/help_child/static/js/matsuoka.js
+++ venv_help_child/help_child/static/js/matsuoka.js
@@ -27,76 +27,12 @@ $(function () {
 });
 
 /* ----------絞り込み検索----------- */
-$(function () {
-    searchWord = function () {
-        var searchResult,
-            searchText = $(this).val(), // 検索ボックスに入力された値
-            targetText,
-            hitNum;
-
-        // 検索結果を格納するための配列を用意
-        searchResult = [];
-
-        // 検索結果エリアの表示を空にする
-        $('#search-result__list').empty();
-        $('.search-result__hit-num').empty();
-
-        // 検索ボックスに値が入ってる場合
-        if (searchText != '') {
-            $('.target-area li').each(function () {
-                targetText = $(this).text();
-
-                // 検索対象となるリストに入力された文字列が存在するかどうかを判断
-                if (targetText.indexOf(searchText) != -1) {
-                    // 存在する場合はそのリストのテキストを用意した配列に格納
-                    searchResult.push(targetText);
-                }
-            });
-
-            // 検索結果をページに出力
-            for (var i = 0; i < searchResult.length; i++) {
-                $('<span>').text(searchResult[i]).appendTo('#search-result__list');
-            }
-
-            // ヒットの件数をページに出力
-            hitNum = '<span>検索結果</span>：' + searchResult.length + '件見つかりました。';
-            $('.search-result__hit-num').append(hitNum);
-        }
-    };
-
-    // searchWordの実行
-    $('#search-text').on('input', searchWord);
-});
-
-$(function () {
-    searchWord = function () {
-        var searchText = $(this).val(), // 検索ボックスに入力された値
-            targetText;
-
-        $('.target-area li').each(function () {
-            targetText = $(this).text();
-
-            // 検索対象となるリストに入力された文字列が存在するかどうかを判断
-            if (targetText.indexOf(searchText) != -1) {
-                $(this).removeClass('hidden');
-            } else {
-                $(this).addClass('hidden');
-            }
-        });
-    };
-
-    // searchWordの実行
-    $('#search-text').on('input', searchWord);
-});
-
-/* 絞り込み機能-part2 */
-
 function myFunction() {
     // Declare variables
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('myInput');
+    input = document.getElementById('search-text');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
+    ul = document.getElementById("list");
     li = ul.getElementsByTagName('li');
 
     // Loop through all list items, and hide those who don't match the search query
