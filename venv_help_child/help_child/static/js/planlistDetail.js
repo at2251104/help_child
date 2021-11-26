1f5804855461deec2c8f80653220d6a03f9e3846
at2251104 <93749622+at2251104@users.noreply.github.com>
2021-11-26 10:18:02 +0900

    planListDetailの修正

--- venv_help_child/help_child/static/js/planlistDetail.js
+++ venv_help_child/help_child/static/js/planlistDetail.js
@@ -1,13 +1,11 @@
 $(function () {
-  document.getElementById("target").src="{% static /images/calender2.png %}";
-
-  $('#target').datepicker({
+  $("#target").datepicker({
     changeYear: true,    // 年を表示
     changeMonth: true,   // 月を選択
     // ↓の日付をいじります（過去も見る必要があるため）
     minDate: '-3y +6m', // 前日以前は選択不可
     maxDate: '+3y +6m',  // 1年半後まで選択可
-    buttonImage: "static/images/calender2.png",        // カレンダーアイコン画像
+    buttonImage: "/static/images/calender2.png",        // カレンダーアイコン画像
     buttonText: "カレンダーから選択", // ツールチップ表示文言
     buttonImageOnly: true,           // 画像として表示
     showOn: "both",
