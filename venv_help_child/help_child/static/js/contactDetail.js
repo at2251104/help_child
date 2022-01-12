c4bd59dde04fdfb170ddc942237578b71dd8b136
s_matsuoka <tky2002107@stu.o-hara.ac.jp>
2022-01-12 09:38:59 +0900

    contactDetail,など

--- venv_help_child/help_child/static/js/contactDetail.js
+++ venv_help_child/help_child/static/js/contactDetail.js
@@ -1,54 +1,30 @@
-$(function () {
-    $("#target").datepicker({
-      changeYear: true,    // 年を表示
-      changeMonth: true,   // 月を選択
-      // ↓の日付をいじります（過去も見る必要があるため）
-      minDate: '-3y +6m', // 前日以前は選択不可
-      maxDate: '+3y +6m',  // 1年半後まで選択可
-      buttonImage: "/static/images/calender2.png",        // カレンダーアイコン画像
-      buttonText: "カレンダーから選択", // ツールチップ表示文言
-      buttonImageOnly: true,           // 画像として表示
-      showOn: "both",
-    });
-    $("#from1").datepicker({
-      changeYear: true,    // 年を表示
-      changeMonth: true,   // 月を選択
-      // ↓の日付をいじります（過去も見る必要があるため）
-      minDate: '-3y +6m', // 前日以前は選択不可
-      maxDate: '+3y +6m',  // 1年半後まで選択可
-      buttonImage: "/static/images/calender2.png",        // カレンダーアイコン画像
-      buttonText: "カレンダーから選択", // ツールチップ表示文言
-      buttonImageOnly: true,           // 画像として表示
-      showOn: "both",
-    });
-    $("#to1").datepicker({
-      changeYear: true,    // 年を表示
-      changeMonth: true,   // 月を選択
-      // ↓の日付をいじります（過去も見る必要があるため）
-      minDate: '-3y +6m', // 前日以前は選択不可
-      maxDate: '+3y +6m',  // 1年半後まで選択可
-      buttonImage: "/static/images/calender2.png",        // カレンダーアイコン画像
-      buttonText: "カレンダーから選択", // ツールチップ表示文言
-      buttonImageOnly: true,           // 画像として表示
-      showOn: "both",
-    });
-    var OBJ = $("target");
-    OBJ.position = absolute;
-    //var plc = $("target").position();
-    //console.log(plc);
-  });
-  
-  
-  
-  // $(function() {
-  //   var from1 = $("#from1").datepicker({
-  //         onSelect: function(selectedDate) {
-  //           $("#to1").datepicker("option","minDate",selectedDate);
-  //         }
-  //     });
-  //   var to1 = $("#to1").datepicker({
-  //           onSelect: function(selectedDate ) {
-  //           $("#from1").datepicker("option","maxDate",selectedDate);
-  //         }
-  //     });
-  // });
+const week = ["日", "月", "火", "水", "木", "金", "土"];
+const today = new Date();
+// 月末だとずれる可能性があるため、1日固定で取得
+var showDate = new Date(today.getFullYear(), today.getMonth(), today.getDay());
+
+// 初期表示
+window.onload = function () {
+    showProcess(today, calendar);
+};
+
+// カレンダー内の各要素にリンクを設定、Detailに飛べるようにする
+function OnLinkClick(year,month,count){
+    var n = year * 10000; // YYYY0000
+	n += month * 100 + 100; // YYYYMM00
+	n += count; // YYYYMMDD
+
+    return n;
+}
+
+// 前の日表示
+function prev(){
+    showDate.setMonth(showDate.getDay() - 1);
+    showProcess(showDate);
+}
+
+// 次の日表示
+function next(){
+    showDate.setMonth(showDate.getDay() + 1);
+    showProcess(showDate);
+}
